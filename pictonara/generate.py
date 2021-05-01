4660fd72e27ae425b15847895615c3ee83472d47
Laurent Eriksen <laurent.eriksen@loepfe.com>
2021-05-02 00:06:45 +0200

    Support 3 to 5 players through argparse

--- pictonara/generate.py
+++ pictonara/generate.py
@@ -3,14 +3,20 @@ import numpy as np
 
 def get_card_masks(num_players: int) -> np.ndarray:
     if num_players == 3:
-        masks = np.random.random([num_players, 16])  # TODO
+        masks = np.asarray([[1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+                            [1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ],
+                            [1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, ]])
     elif num_players == 4:
         masks = np.asarray([[1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, ],
                             [1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, ],
                             [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, ],
                             [1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, ]])
     elif num_players == 5:
-        masks = np.random.random([num_players, 16])  # TODO
+        masks = np.asarray([[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+                            [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, ],
+                            [1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, ],
+                            [1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, ],
+                            [1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, ]])
     else:
         raise ValueError("Game currently only supported for 3, 4, or 5 players.")
 
--- tests/mask_tests.py
+++ tests/mask_tests.py
@@ -6,25 +6,25 @@ from pictonara.generate import get_card_masks
 class MaskTest(unittest.TestCase):
     def test_card_masks_are_random(self):
         np.random.seed(42)
-        for num_players in [4]:
+        for num_players in [3, 4, 5]:
             masks1 = get_card_masks(num_players=num_players)
             masks2 = get_card_masks(num_players=num_players)
             self.assertFalse((masks1 == masks2).all())
 
     def test_each_player_has_five_cards_marked(self):
-        for num_players in [4]:
+        for num_players in [3, 4, 5]:
             masks = get_card_masks(num_players=num_players)
             num_players_with_five_marked = len(np.where(np.sum(masks, axis=1) == 5)[0])
             self.assertEqual(num_players_with_five_marked, num_players)
 
     def test_card_masks_all_overlap_in_exactly_one_spot(self):
-        for num_players in [4]:
+        for num_players in [3, 4, 5]:
             masks = get_card_masks(num_players=num_players)
             num_complete_overlaps = len(np.where(np.sum(masks, axis=0) == num_players)[0])
             self.assertEqual(num_complete_overlaps, 1)
 
     def test_two_individual_card_masks_overlap_in_more_than_one_spot(self):
-        for num_players in [4]:
+        for num_players in [3, 4, 5]:
             masks = get_card_masks(num_players=num_players)
             for i, m1 in enumerate(masks):
                 for j, m2 in enumerate(masks):
